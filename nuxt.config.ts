4af28246301a17d70559177268a5dfe1f3bcf6af
Justin <justin-urbanek@bluewin.ch>
2024-11-04 10:59:50 +0100

    feat: font awesome module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,36 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
-  modules: ['@nuxtjs/tailwindcss'],
+  // https://nuxt.com/docs/guide/directory-structure/composables
+  imports: {
+    dirs: ["composables/**"],
+  },
+
+  modules: ['@vesp/nuxt-fontawesome', '@nuxtjs/tailwindcss'],
+
+  // https://nuxt.com/modules/nuxt-fontawesome
+  fontawesome: {
+    // https://github.com/bezumkin/nuxt-fontawesome/tree/main
+    component: "fa",
+    suffix: true,
+    icons: {
+      solid: [
+        "bars",
+        "times",
+        "arrow-left",
+        "arrow-right",
+        "arrow-down",
+        "book",
+        "copyright",
+        "hand-pointer",
+        "arrow-up",
+        "external-link-square-alt",
+        "copy",
+        "link",
+      ],
+      regular: ["user"],
+    },
+  },
 
   tailwindcss: {
     cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
