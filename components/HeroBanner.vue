062915044695e250ff73dfc720b361f148ed6f55
Justin <justin-urbanek@bluewin.ch>
2024-11-05 08:31:09 +0100

    refactor: gray desc

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -4,7 +4,7 @@
       <h1 class="text-neutral2 dark:text-darkNeutral2 my-8">
         heading 1
       </h1>
-      <p class="text-neutral2 dark:text-darkNeutral2 w-3/4 mx-auto">
+      <p class="text-neutral2 dark:text-gray1 w-3/4 mx-auto">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo eum laboriosam nisi et, alias culpa officia
         neque omnis facilis impedit hic maiores rem temporibus quaerat quia corrupti ullam ad ipsam.
       </p>
