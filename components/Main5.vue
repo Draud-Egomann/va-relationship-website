84ccf203ca44f5f4a3292eb45a25f264dbd354f1
Justin <justin-urbanek@bluewin.ch>
2024-11-05 10:19:40 +0100

    feat: video banner comp

---
+++ components/Main5.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="max-w-6xl mx-auto p-8">
+    <div class="py-24 text-center">
+      <h2 class="text-neutral2 dark:text-darkNeutral2 my-8">
+        heading 2
+      </h2>
+      <p class="text-neutral2 dark:text-gray1 w-3/4 mx-auto">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo eum laboriosam nisi et, alias culpa officia
+        neque omnis facilis impedit hic maiores rem temporibus quaerat quia corrupti ullam ad ipsam.
+      </p>
+    </div>
+
+    <!-- video container -->
+    <div class="flex justify-center gap-8">
+      <video controls class="w-full h-full object-cover mx-auto">
+        <source src="https://videos.pexels.com/video-files/29110495/12575725_2560_1440_50fps.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  </div>
+</template>
