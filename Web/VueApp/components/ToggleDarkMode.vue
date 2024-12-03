8205b79dec1479c26e9925ca4103f9a8a0880197
Justin <justin-urbanek@bluewin.ch>
2024-12-03 09:05:43 +0100

    feat: page has darkmode as default

--- Web/VueApp/components/ToggleDarkMode.vue
+++ Web/VueApp/components/ToggleDarkMode.vue
@@ -3,18 +3,18 @@
 const isDarkMode = ref(false)
 
 onMounted(() => {
+  // Default to dark mode if no preference is stored in localStorage
+  const prefersDarkMode = !('theme' in localStorage) || localStorage.theme === 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches
+
   // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-  document.documentElement.classList.toggle(
-    'dark',
-    localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-  )
+  document.documentElement.classList.toggle('dark', prefersDarkMode)
+  localStorage.theme = prefersDarkMode ? 'dark' : 'light'
 
   isDarkMode.value = document.documentElement.classList.contains('dark')
 })
 
 function toggleDarkMode() {
-  document.documentElement.classList.toggle('dark')
-
+  document.documentElement.classList.toggle('dark', isDarkMode.value)
   localStorage.theme = isDarkMode.value ? 'dark' : 'light'
 }
 </script>
