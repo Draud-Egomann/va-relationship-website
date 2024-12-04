d1d4f2bcc80130bd44b04f4e9ec0130ed169715c
egomann88 <justin-urbanek@bluewin.ch>
2024-12-04 23:17:55 +0100

    feat: imprint page

---
+++ Web/VueApp/pages/Legal/imprint.md
@@ -0,0 +1,28 @@
+# Impressum
+
+Zuletzt aktualisiert am: 04.12.2024
+
+## Verantwortliche Instanz(en)
+
+- Dominic Toksu & Justin Urbanek  
+- Schweiz  
+- **E-Mail:** [l1rsfg3jo{at}mozmail{dot}com](mailto:l1rsfg3jo@mozmail.com)
+
+## Haftungsausschluss
+
+Die verantwortliche(n) Instanz(en) übernimmt keine Gewähr für die Richtigkeit, Genauigkeit, Aktualität, Zuverlässigkeit und Vollständigkeit der Informationen.  
+Haftungsansprüche gegen den Autor wegen Schäden materieller oder immaterieller Art, die aus dem Zugriff oder der Nutzung bzw. Nichtnutzung der veröffentlichten Informationen, durch Missbrauch der Verbindung oder durch technische Störungen entstanden sind, werden ausgeschlossen.
+
+Alle Angebote sind freibleibend. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.
+
+## Haftungsausschluss für Inhalte und Links
+
+Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs. Es wird jegliche Verantwortung für solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher Webseiten erfolgen auf eigene Gefahr des jeweiligen Nutzers.
+
+## Datenschutz
+
+Informationen zur Verarbeitung personenbezogener Daten und zu Ihren Rechten finden Sie in unserer [Datenschutzerklärung](./privacy.md).
+
+## Urheberrechtserklärung
+
+Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf dieser Website, gehören ausschliesslich den verantwortliche(n) Instanz(en) oder den speziell genannten Rechteinhabern. Für die Reproduktion jeglicher Elemente ist die schriftliche Zustimmung des Urheberrechtsträgers im Voraus einzuholen.
---
+++ Web/VueApp/pages/Legal/imprint.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="max-w-6xl mx-auto p-8">
+    <h1 class="text-4xl font-bold mb-4">Impressum</h1>
+    <p class="mb-4">Zuletzt aktualisiert am: 04.12.2024</p>
+
+    <h2 class="text-2xl font-semibold mt-6">Verantwortliche Instanz(en)</h2>
+    <p class="mb-4">
+      - Dominic Toksu & Justin Urbanek<br>
+      - Schweiz<br>
+      - <strong>E-Mail:</strong>
+      <NuxtLink to="mailto:l1rsfg3jo@mozmail.com" class="text-blue-500 underline ml-2">
+        l1rsfg3jo{at}mozmail{dot}com
+      </NuxtLink>
+    </p>
+
+    <h2 class="text-2xl font-semibold mt-6">Haftungsausschluss</h2>
+    <p class="mb-4">
+      Die verantwortliche(n) Instanz(en) übernimmt keine Gewähr für die Richtigkeit, Genauigkeit, Aktualität,
+      Zuverlässigkeit und Vollständigkeit der Informationen. Haftungsansprüche gegen den Autor wegen Schäden materieller
+      oder immaterieller Art, die aus dem Zugriff oder der Nutzung bzw. Nichtnutzung der veröffentlichten Informationen,
+      durch Missbrauch der Verbindung oder durch technische Störungen entstanden sind, werden ausgeschlossen.
+    </p>
+    <p class="mb-4">
+      Alle Angebote sind freibleibend. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte
+      Angebot
+      ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder
+      endgültig
+      einzustellen.
+    </p>
+
+    <h2 class="text-2xl font-semibold mt-6">Haftungsausschluss für Inhalte und Links</h2>
+    <p class="mb-4">
+      Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs. Es wird jegliche
+      Verantwortung für solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher Webseiten erfolgen auf eigene
+      Gefahr des jeweiligen Nutzers.
+    </p>
+
+    <h2 class="text-2xl font-semibold mt-6">Datenschutz</h2>
+    <p class="mb-4">
+      Informationen zur Verarbeitung personenbezogener Daten und zu Ihren Rechten finden Sie in unserer
+      <NuxtLink to="/legal/privacy" class="text-blue-500 underline">Datenschutzerklärung</NuxtLink>.
+    </p>
+
+    <h2 class="text-2xl font-semibold mt-6">Urheberrechtserklärung</h2>
+    <p class="mb-4">
+      Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf dieser Website, gehören
+      ausschliesslich den verantwortliche(n) Instanz(en) oder den speziell genannten Rechteinhabern. Für die
+      Reproduktion
+      jeglicher Elemente ist die schriftliche Zustimmung des Urheberrechtsträgers im Voraus einzuholen.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+h1,
+h2,
+h3,
+ul,
+p {
+  @apply text-neutral2 dark:text-darkNeutral2;
+}
+
+h1 {
+  @apply dark:text-darkSpecial1Dark
+}
+
+h2 {
+  @apply dark:text-darkPrimary1Dark;
+}
+
+h3 {
+  @apply dark:text-darkPrimary1Dark;
+}
+</style>
