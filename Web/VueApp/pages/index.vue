a3d7958b953a1a44fa63dcda7873d12ddc2169c5
egomann88 <justin-urbanek@bluewin.ch>
2024-12-04 23:37:23 +0100

    feat: delete ToggleDarkMode component as its no logger needed

--- Web/VueApp/pages/index.vue
+++ Web/VueApp/pages/index.vue
@@ -12,7 +12,6 @@ const tableData = DataProvider.GetTableData();
 </script>
 
 <template>
-  <ToggleDarkMode />
   <HeroBanner :heading-type="1" :title="'Heading 1'"
     :text="'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'" />
 
