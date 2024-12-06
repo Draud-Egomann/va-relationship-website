c4b8a5e5b35e09aebb154ce7ea84469982b0cd2c
Justin <justin-urbanek@bluewin.ch>
2024-12-06 08:07:14 +0100

    feat: cta button better formatting on mobile

--- Web/VueApp/components/Call2Action.vue
+++ Web/VueApp/components/Call2Action.vue
@@ -18,11 +18,11 @@ const emit = defineEmits(['onButtonClicked']);
       <div class="relative p-6 bg-accent1 dark:bg-darkAccent1Dark rounded-lg shadow-lg">
         <div class="flex justify-between">
 
-          <div class="w-4/5 mb-4">
+          <div class="w-3/5 md:w-4/5 mb-4">
             <Heading :heading-type="4" :title="title" :text="text" />
           </div>
 
-          <div class="w-1/5 h-fit flex justify-center m-auto">
+          <div class="w-2/5 md:w-1/5 h-fit flex justify-center m-auto">
             <button @click="emit('onButtonClicked')"
               class="bg-primary1 dark:bg-darkPrimary1Dark text-white font-medium py-2 px-4 rounded-md hover:bg-opacity-90 transition duration-300">
               {{ ctaText }}
