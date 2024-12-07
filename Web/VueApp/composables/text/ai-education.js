31ad15def7508b4b16d50615576ab3a2a30d1ee8
dominictosku <dominic.tosku@gmail.com>
2024-12-07 11:45:19 +0100

    feat: add education page

---
+++ Web/VueApp/composables/text/ai-education.js
@@ -0,0 +1,45 @@
+export const mainText = `In vielen Schulklassen ist es schon Standard geworden, die KI um Hilfe zu fragen.
+Bewusst sind sich die Lehrer dem meist nicht und bewerten die Arbeit im Glauben, sie 
+sei durch menschliche Leistung entstanden. Doch welchen Nutzen bietet KI und wo ist 
+ihr Limit?
+Den grössten Nutzen der KI sehen viele in der Geschwindigkeit und den einfach zu 
+verstehenden Texten. Innerhalb von Sekunden bekommt man einfache Ansätze. Hier 
+stösst man schon auf die ersten Hindernisse. Die Antworten der KI können nämlich 
+sehr schnell falsch sein, obwohl die KI dies zuversichtlich anders behauptet. Dieses 
+Phänomen nennt man „Halluzinieren“ in der digitalen Welt. Doch wie kommt es dazu?`;
+
+export const sections = [
+    {
+      title: 'KI und denken',
+      text: `Eine KI kann grundsätzlich nicht logisch denken wie wir Menschen. Sie wird mit 
+Unmengen an Daten, wodurch sie Muster erkennt und darauf basierend antwortet. 
+Wenn ich die Bücher von Shakespeare zum Trainieren benutze, gibt die KI Antworten 
+mit ähnlichem Schreibstil zurück. Innovationen oder eigenständige Ideen sind jedoch 
+selten, da die KI meist den Durchschnitt aller Trainingsdaten oder bereits existierende 
+Lösungen liefert. Für Schulaufgaben mit definierten Lösungen ist dies perfekt geeignet 
+und funktioniert oft auch noch auf Hochschulebene. In der Arbeitswelt jedoch, wo 
+Innovationen gefragt sind, stösst die KI schnell an ihre Grenzen. Dies kann stark 
+variieren nach Berufung, zeigt jedoch ein grundlegendes Problem auf, wenn eine 
+Generation nicht genug gebildet ist, um diese Probleme anzugehen`
+    },
+    {
+      title: 'Das menschliche Gehirn',
+      text: `Es gibt bereits KI-Modelle, die das neurale Netzwerk des Gehirns nachstellen sollen, 
+sogenannte künstliche neuronale Netzwerke. Doch auch diese bringen 
+Herausforderungen mit sich. Auch Lehrpläne müssen angepasst werden. MultipleChoice-Aufgaben stellen kaum noch eine Herausforderung für moderne Modelle dar.
+Stattdessen sollten Aufgaben das kritische Denken fördern. Wissensfragen verlieren 
+an Bedeutung; eigene Lösungsansätze werden wichtiger. Dies führt zu einer stärkeren 
+Belastung der Schüler durch den höheren Schwierigkeitsgrad.`
+    },
+    {
+      title: 'Dilemma der Lehrer',
+      text: `Ein Bericht beschreibt, wie Lehrer zunehmend mündlichen Prüfungen nutzen, um 
+Kompetenzen zu prüfen. Diese sind zwar aufwendiger, verhindern jedoch Schummeln 
+und sichern das Wissen der Schüler (1)
+Die KI hat grosses Potenzial beim personalisierten Lernen. Sie kann schnell Feedback 
+geben und bei Lernproblemen helfen, wenn die Lehrkraft nicht verfügbar ist. Auch gibt 
+es vielleicht Fragen, die man sich nicht traut, der Klasse zu stellen. Wichtig sind hierbei 
+ein gesundes Misstrauen und ein Faktencheck, ob die Antwort wirklich relevant ist.
+`
+    },
+  ]
--- Web/VueApp/pages/ai-education/index.vue
+++ Web/VueApp/pages/ai-education/index.vue
@@ -1,5 +1,21 @@
-<template>
+<script lang="ts" setup>
+import { mainText, sections } from '~/composables/text/ai-education';
+const heroBanner = {
+  title: "KI in der Bildung: Ein Segen oder der Untergang?",
+  subTitle: "Die neue Intimität",
+  isBigView: true,
+  paragraphs: [
+    "Ist unsere Bildung gefährdet?",
+  ],
+}
 
+</script>
+
+<template>
+  <HeroBanner :title="heroBanner.title" :subTitle="heroBanner.subTitle" :isBigView="heroBanner.isBigView"
+    :hasButton="false" :buttonText="null" :paragraphs="heroBanner.paragraphs" @buttonClickEvent="() => { }" />
+
+  <Texts :title="'KI in der Bildung: Ein Segen oder der Untergang?'" :text=mainText :sections=sections />
   <!-- KI in der Bildung: Ein Segen oder der Untergang? -->
   <!-- Die KI von Heute -->
   <!-- KI vor Gericht -->
